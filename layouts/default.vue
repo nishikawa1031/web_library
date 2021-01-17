174a3c6cb6fcd71f3af228baaca17d3151711bd4
nishikawa1031 <tatsunishitatsu@gmail.com>
2021-01-17 21:18:19 +0900

    headerとfooterが完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,116 +1,84 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+  <v-app>
+    <v-app-bar fixed app>
+      <v-toolbar-title>
+        <v-layout>
+          <v-flex lg12>
+              <!-- <v-col><Logo /></v-col> -->
+              <v-btn text to="/">languages</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn text to="/login">{{ 'ログイン' }}</v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
     </v-app-bar>
-    <v-main>
+    <v-content>
       <v-container>
         <nuxt />
       </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    </v-content>
+    <v-footer>
+      <v-row>
+        <v-col lg="1">
+          <v-row><v-btn text to="">About Us</v-btn></v-row>
+          <v-row><v-btn text to="">News</v-btn></v-row>
+          <v-row><v-btn text to="">Contact Us</v-btn></v-row>
+        </v-col>
+        <v-col lg="1">
+
+        </v-col>
+        <v-col lg="1">
+          <v-row><v-btn text to="">Privacy Policy</v-btn></v-row>
+          <v-row><v-btn text to="">Terms of Use</v-btn></v-row>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+// import firebase from '@/plugins/firebase'
+
+import Logo from '~/components/Logo.vue'
 export default {
-  data () {
+  asyncData() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      isWaiting: true,
+      isLogin: false,
+    }
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.isWaiting = false
+      if (user) {
+        this.isLogin = true
+        this.user = user
+      } else {
+        this.isLogin = false
+        this.user = []
+      }
+    })
+  },
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      fixed: false
+    }
+  },
+  methods: {
+    googleLogin() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(provider)
+      this.$router.push({path:'/login'})
+    },
+    logOut() {
+      firebase.auth().signOut()
     }
   }
 }
