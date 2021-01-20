d604ce4825575187385d6404118d8e3fbb9ed339
nishikawa1031 <tatsunishitatsu@gmail.com>
2021-01-20 23:05:21 +0900

    ドロップダウンメニューの作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,24 @@
         </v-layout>
       </v-toolbar-title>
       <v-toolbar-items>
-        <v-btn text to="/login" color="white">{{ $t('login') }}</v-btn>
+          <v-btn text to="/login" color="white">{{ $t('login') }}</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+            <v-btn v-on="on" text color="white">民事系</v-btn>
+          </template>
+          <v-list color="blue-grey darken-4">
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title color="white">民法</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title color="white">民訴法</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </v-toolbar-items>
       <v-spacer></v-spacer>
         <v-btn text to="/login" color="white">{{ $t('login') }}</v-btn>
