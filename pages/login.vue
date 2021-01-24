f7e7c338c0f56a18309afd8810ff82f2e6cdb2a2
nishikawa1031 <tatsunishitatsu@gmail.com>
2021-01-24 10:50:14 +0900

    aboutページの作成

--- pages/login.vue
+++ pages/login.vue
@@ -1,80 +1,22 @@
 <template>
-  <v-container fluid>
-    <v-layout justify-center>
-      <v-card
-        v-if="isWaiting"
-        class="mx-auto"
-        max-width="500"
-        outlined
-      >
-        <p>{{ $t('loading') }}</p>
-      </v-card>
-      <div v-else>
-        <v-card
-          v-if="!isLogin"
-          class="mx-auto"
-          max-width="500"
-          outlined
-        >
-          <v-btn @click="googleLogin">{{ $t('googlelogin') }}</v-btn>
-        </v-card>
-      <v-card
-        v-else
-        class="mx-auto"
-        max-width="500"
-        outlined
-      >
-      <v-card-title>
-        {{ $t('sing_up') }}
-      </v-card-title>
-        <v-card-actions>
-          <v-btn @click="submit()">
-            {{$t('submit')}}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-      <br>
-      </div>
-    <v-row>
-      <v-btn v-if="isLogin" @click="logOut">{{$t('logout')}}</v-btn>
-    </v-row>
-    </v-layout>
-  </v-container>
+  <login-navigation />
 </template>
 
 <script>
-import firebase from '@/plugins/firebase'
-
+import LoginNavigation from '~/components/LoginNavigation.vue';
 export default {
   asyncData() {
     return {
-      isWaiting: true,
-      isLogin: false,
-      user: []
     }
   },
+  components: { LoginNavigation },
   mounted() {
-    firebase.auth().onAuthStateChanged((user) => {
-      this.isWaiting = false
-      if (user) {
-        this.isLogin = true
-        this.user = user
-      } else {
-        this.isLogin = false
-        this.user = []
-      }
-    })
-  },
-  methods: {
-    googleLogin() {
-      const provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithRedirect(provider)
-    },
-    logOut() {
-      firebase.auth().signOut()
-    },
-  },
+    console.log(LoginNavigation)
+    // console.log(isLogin)
+  }
 }
+
+
 </script>
 <style scoped>
   li {
