c730a96b51d2225a9c4010ed74c2498dd032a928
nishikawa1031 <tatsunishitatsu@gmail.com>
2021-02-10 00:29:43 +0900

    不要なコードの削除

--- components/Article.vue
+++ components/Article.vue
@@ -16,33 +16,10 @@
 export default {
   data () {
     return {
-        // 存在するページの西暦を入れた配列
-        years: [
-            1995,
-            1996,
-            1997,
-            1998,
-            1999,
-            2000,
-            2001,
-            2002,
-            2003,
-            2004,
-            2005,
-        ]
     };
   },
 
-  methods: {
-    // 引数で西暦を受け取り、
-    // 2000年（平成12年）の形式の文字列にして返す
-    exportYear: function (year) {
-        const date = new Date(year, 3, 1);
-        const options = {era: 'long'};
-        const jaEra = new Intl.DateTimeFormat('ja-JP-u-ca-japanese', options).format(date);
-        const yearText = year + "年（" + jaEra.slice(0, -4) + "）"
-        return yearText
-    }
+  methods: {s
   }
 }
 </script>
